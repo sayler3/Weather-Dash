8688b1375e208a678681e0e4375f5d5e8c166761
Sam Ayler <sayler8701@gmail.com>
2020-11-04 20:23:23 -0800

    adding fetch api to app.js file

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,15 @@
-console.log("hello");
+// console.log("hello");
+
+// function getApi() {
+	let requestUrl =
+		"http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=imperial&lang=en&appid=a1e39e520ce0eb06a39ade64faa243cd";
+
+	fetch(requestUrl)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+        });
+        
+// }
