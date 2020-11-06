985abbc9c2f2bdee6c8019ffc16d176a881c4055
Sam Ayler <sayler8701@gmail.com>
2020-11-06 12:04:43 -0800

    adding function for city search field and fetch

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,30 @@
-// console.log("hello");
+$(document).ready(function () {
+	$("form").on("submit", function (e) {
+        e.preventDefault();
+        let cityInput = e.target.querySelector('input').value;
+        console.log(cityInput);
 
-// function getApi() {
-	let requestUrl =
-		"http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=imperial&lang=en&appid=a1e39e520ce0eb06a39ade64faa243cd";
-
-	fetch(requestUrl)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (data) {
-			console.log(data);
-        });
+        function getApi() {
+            // event.preventDefault();
+            
+            // Storing api key in a constant varible
+            const key = "&appid=a1e39e520ce0eb06a39ade64faa243cd";
         
-// }
+            let requestUrl =
+                "http://api.openweathermap.org/data/2.5/weather?q=" +
+                cityInput +
+                "&units=imperial&lang=en" +
+                key;
+        
+            // fetch request for api data
+            fetch(requestUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                });
+        }
+            getApi()
+    });
+});
