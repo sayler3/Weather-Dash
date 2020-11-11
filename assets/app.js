8190865c380175e98b568afa216b03920f908696
Sam Ayler <sayler8701@gmail.com>
2020-11-11 12:41:14 -0800

    fixed id's for forecast cards in index.html and added dispay forecast fucntion for focasted weather divs

--- assets/app.js
+++ assets/app.js
@@ -67,36 +67,44 @@ $(document).ready(function () {
 			})
 			.then(function (data) {
 				console.log(data);
-				dispayWeather(data);
+                dispayWeather(data);
+                displayForcast(data);
 				let iconCode = data.current.weather[0].icon;
-				let iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-                $("#cwicon").attr("src", iconUrl);
+				let iconUrl =
+					"http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+				$("#cwicon").attr("src", iconUrl);
 			});
 	}
-    // function for displaying current weather data to page
-	const dispayWeather = function (data) {
+	// function for displaying current weather data to page
+	function dispayWeather(data) {
 		$("#cTemperature").text(data.current.temp + " F");
 		$("#cHumidity").text(data.current.humidity + " %");
 		$("#wind").text(data.current.wind_speed + " MPH");
-        $("#index").text(data.current.uvi);
-        if(data.current.uvi >= 6) {
-            $('#index').css('background-color', 'red');
-            $('#index').css('color', 'white');
-        } else if(data.current.uvi >= 3) {
-            $('#index').css('background-color', 'orange');
-            $('#index').css('color', 'white');
-        } else {
-            $('#index').css('background-color', 'light-green');
-            $('#index').css('color', 'white');
+		$("#index").text(data.current.uvi);
+		if (data.current.uvi >= 6) {
+			$("#index").css("background-color", "red");
+			$("#index").css("color", "white");
+		} else if (data.current.uvi >= 3) {
+			$("#index").css("background-color", "orange");
+			$("#index").css("color", "white");
+		} else {
+			$("#index").css("background-color", "light-green");
+			$("#index").css("color", "white");
+		}
+	}
+    // function for displaying forecasted weather 5 days
+	function displayForcast(data) {
+		for (i = 0; i < 5; i++) {
+			let iconCode = data.daily[i].weather[0].icon;
+			let iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+			let addDay = i + 1
+			let fDay = moment(date, "MM/DD/YYYY")
+				.add(addDay, 'd')
+				.format("MM/DD/YYYY");
+			$(`#forecast_${i}_title`).text(fDay);
+			$(`#forecast_${i}_img`).attr("src", iconUrl);
+			$(`#forecast_${i}_temp`).text(data.daily[i].temp.max + " F");
+            $(`#forecast_${i}_hum`).text(data.daily[i].humidity + " %");
         }
-	};
-
-	// let getCoord = function (data) {
-	//     let cityLat = data.coord.lat;
-	//     let cityLong = data.coord.lon;
-	//     let iconCode = data.weather[0].icon;
-	//     let iconUrl= "http://openweathermap.org/img/w/" + iconCode + ".png";
-	//     $('#cwicon').attr("scr", iconUrl);
-
-	// }
+	}
 });
