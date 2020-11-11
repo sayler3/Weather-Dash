53e062cd7aaf5c9a750ef94f2bca1ad235c8dfc3
Sam Ayler <sayler8701@gmail.com>
2020-11-11 10:21:03 -0800

    fixing fetch function for forcast and uv index

--- assets/app.js
+++ assets/app.js
@@ -1,44 +1,90 @@
+
+
 $(document).ready(function () {
+    let date = moment().format("MM/DD/YYYY");
+	let startUp = [
+		"San Francisco",
+		"Oakland",
+		"Atlanta",
+		"Chicago",
+		"New York",
+		"Seattle",
+		"Denver",
+		"Stockholm",
+	];
+	for (i = 0; i < startUp.length; i++) {
+		$(`#recent_city_${i}`).html(startUp[i]);
+	}
+	// if (window.localStorage.length === 0) {
+
+    // }
+    
+    // on submit function with prevent default for form user input
 	$("form").on("submit", function (e) {
 		e.preventDefault();
-		let cityInput = e.target.querySelector("input").value;
+		let cityInput = $("input").val();
 		console.log(cityInput);
-
-		function getApi() {
-			// event.preventDefault();
-
-			// Storing api key in a constant varible
-			const key = "&appid=a1e39e520ce0eb06a39ade64faa243cd";
-
-			let requestUrl =
-				"http://api.openweathermap.org/data/2.5/weather?q=" +
-				cityInput +
-				"&units=imperial&lang=en" +
-				key;
-
-			// fetch request for api data
-			fetch(requestUrl)
-				.then(function (response) {
-					return response.json();
-				})
-				.then(function (data) {
-					console.log(data);
-				});
-
-			let forcast =
-				"http://api.openweathermap.org/data/2.5/forecast?q=" +
-				cityInput +
-				"&units=imperial&lang=en" +
-				key;
-
-			fetch(forcast)
-				.then(function (response) {
-					return response.json();
-				})
-				.then(function (data) {
-					console.log(data);
-				});
-		}
-		getApi();
+        getApi(cityInput);
+        // getForecast(cityLat, cityLong)
 	});
+
+	function getApi(cityInput) {
+		// Storing api key in a constant varible
+		const key = "&appid=a1e39e520ce0eb06a39ade64faa243cd";
+
+		let requestUrl =
+			"http://api.openweathermap.org/data/2.5/weather?q=" +
+			cityInput +
+			"&units=imperial&lang=en" +
+			key;
+		// fetch request for api data
+		fetch(requestUrl)
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (data) {
+                console.log(data);
+                let cityLat = data.coord.lat;
+                let cityLong = data.coord.lon;
+                console.log(cityLat, cityLong);
+                getForecast(cityLat, cityLong);
+                $('#city_name').text(data.name + " ("+ date + ")");
+            });
+    }
+    
+    function getForecast(cityLat, cityLong) {
+        
+        const key = "&appid=a1e39e520ce0eb06a39ade64faa243cd";
+        
+        let forcast =
+			"https://api.openweathermap.org/data/2.5/onecall?lat=" +
+			cityLat + "&lon=" + cityLong +
+			"&units=imperial&lang=en" +
+            key;
+            
+		fetch(forcast)
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (data) {
+                console.log(data);
+                dispayWeather(data);
+			});
+    }
+
+    const dispayWeather = function(data) {
+        $('#cTemperature').text(data.current.temp + " F");
+        $('#cHumidity').text(data.current.humidity + " %");
+        $('#wind').text(data.current.wind_speed + " MPH");
+        $('#index').text(data.current.uvi);
+    }
+
+    // let getCoord = function (data) {
+    //     let cityLat = data.coord.lat;
+    //     let cityLong = data.coord.lon;
+    //     let iconCode = data.weather[0].icon;
+    //     let iconUrl= "http://openweathermap.org/img/w/" + iconCode + ".png";
+    //     $('#cwicon').attr("scr", iconUrl);
+
+    // }
 });
